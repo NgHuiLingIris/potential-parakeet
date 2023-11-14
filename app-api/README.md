2d5d0e212716932a56ff5f5f1bd2d47f1b74f9c7
Ng Hui Ling Iris <irisnghl@gmail.com>
2023-11-14 22:42:19 +0800

    cleanup for just monitoring files

--- app-api/README.md
+++
@@ -1,4 +0,0 @@
-This is a boilerplate API template meant to demonstrate the back-end server after keycloak authentication.
-
-To run
-`npm install && npm start`
--- app-api/controllers/finance-controller.js
+++
@@ -1,5 +0,0 @@
-const getFinanceCurrentBalanceForUser = async (req, res, next) => {
-    res.status(200).json({"amount":500});
-};
-
-exports.getFinanceCurrentBalanceForUser = getFinanceCurrentBalanceForUser;
